cbc95ff0550b7b083ac2d32a94b4cac0ebdffad4
CMGS <ilskdw@gmail.com>
2011-11-04 08:29:25 +0000

    add app.py

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
-# a dummy wsgi app
-def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return ["hello, world!"]
+import os
+import sys
+import traceback
+from django.core.handlers.wsgi import WSGIHandler
+from django.core.signals import got_request_exception
+from django.core.management import call_command
+
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'djopenid'))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'djopenid.settings'
+
+def exception_printer(sender, **kwargs):
+        traceback.print_exc()
+
+got_request_exception.connect(exception_printer)
+call_command('syncdb')
+app = WSGIHandler()
