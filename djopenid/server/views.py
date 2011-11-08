cd5bfee7580f9ded1e03dd9aab7d9762c26f3a85
CMGS <ilskdw@gmail.com>
2011-11-08 08:25:46 +0000

    debug

--- djopenid/server/views.py
+++ djopenid/server/views.py
@@ -262,6 +262,7 @@ def processTrustResult(request):
            openid_request.trust_root not in request.session['auth_sites']:
             print 123
             request.session['auth_sites'].append(openid_request.trust_root)
+            print request.session.get('auth_sites', None), '&&&&&'
         else:
             print 456
             request.session['auth_sites'] = [openid_request.trust_root, ]
