2f1b67392f1fe7a40b61cbdf78084c25258b81f7
CMGS <ilskdw@gmail.com>
2012-03-27 10:55:56 +0000

    test daemons database connections

---
+++ testd.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'djopenid.settings'
+
+import time
+from djopenid.server.models import AuthSites
+
+if __name__ == '__main__':
+    fp = file('test.log', 'w')
+    i = 0
+    print >>fp, '\n'
+    while True:
+        print >>fp, 'starting...'
+        for row in AuthSites.objects.all():
+            print >>fp, i, row.id
+            fp.flush()
+        print >>fp, 'done.\n'
+        i += 1
+        time.sleep(5)
